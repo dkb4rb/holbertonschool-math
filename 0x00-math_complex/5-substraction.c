3df753158493eaaec62b93447b7f1844d9850674
DKBARB10 <jjuannca651@gmail.com>
2021-08-02 01:48:32 -0500

    0x00-math_complex_Substractions_Values_Complex_Numbers

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * substraction - substrction of the value numbers complex c1 + c2 in c3
+ * @c1: first complex number
+ * @c2: second complex number
+ * @c3: sum of complex numbers
+ * Return: void
+ */
+
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
